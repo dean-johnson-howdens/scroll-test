0ecbe88d7ca805f6f8eefa961892d235f1c68c42
Dean Johnson <dean.johnson@howdens.com>
2026-06-04 12:36:59 +0100

    Modified dvh on body as well

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="main-window">
-      <body>{children}</body>
+      <body className="main-window">{children}</body>
     </html>
   );
 }
